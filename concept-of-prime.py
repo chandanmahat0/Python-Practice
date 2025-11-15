6e4860a62d22a617d22d24c6e283907f7a307710
Chandan Kumar Mahato <chandannmahatoo7@gmail.com>
2025-11-15 18:17:35 +0530

    Write a program to check whether a number is prime

---
+++ concept-of-prime.py
@@ -0,0 +1,12 @@
+# Write a program to check whether a number is prime.
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+num = int(input("Enter number: "))
+print("Prime" if is_prime(num) else "Not Prime")
