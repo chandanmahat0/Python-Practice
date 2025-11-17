03d69b30c6dc2e5dd4ae96b9cefbfbc0c19d99ca
Chandan Kumar Mahato <chandannmahatoo7@gmail.com>
2025-11-17 20:47:33 +0530

    Find intersection of multiple sets.

---
+++ intersectionSets.py
@@ -0,0 +1,5 @@
+# 13. Find intersection of multiple sets.
+s1 = set(map(int, input("Enter set 1: ").split()))
+s2 = set(map(int, input("Enter set 2: ").split()))
+s3 = set(map(int, input("Enter set 3: ").split()))
+print(s1 & s2 & s3)
